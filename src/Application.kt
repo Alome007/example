078491619ddcdb052cd772edea168a6e044659bf
alome007 <astrocoder007@gmail.com>
2020-10-24 00:33:13 +0100

    Added Routes

--- src/Application.kt
+++ src/Application.kt
@@ -1,14 +1,18 @@
 package com.first
 
 import io.ktor.application.*
+import io.ktor.http.ContentType
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    
+    routing{
+        this.users()
+
+    }
 }
 
